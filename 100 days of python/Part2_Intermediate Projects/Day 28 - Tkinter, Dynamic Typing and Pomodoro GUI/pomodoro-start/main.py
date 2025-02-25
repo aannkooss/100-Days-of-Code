b8cdeffea908125825b537d376e7af0f93e6cd96
Ankush Joshi <joshiankush6@gmail.com>
2025-02-24 22:16:51 -0800

    Day 27 Done

--- 100 days of python/Part2_Intermediate Projects/Day 28 - Tkinter, Dynamic Typing and Pomodoro GUI/pomodoro-start/main.py
+++ 100 days of python/Part2_Intermediate Projects/Day 28 - Tkinter, Dynamic Typing and Pomodoro GUI/pomodoro-start/main.py
@@ -1,3 +1,7 @@
+# Ankush Joshi
+# February 24, 2025
+# Day 28/100 Pomodoro Timer
+
 from tkinter import *
 import math
 
@@ -10,14 +14,39 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+def resetTimer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timerText, text="00:00")
+    timerLabel.config(text="timer")
+    check.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def startTimer():
+    global reps
+    reps += 1
 
-    countDown(5*60)
+    workSec = WORK_MIN * 60
+    shortBreakSec = SHORT_BREAK_MIN * 60
+    longBreakSec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        countDown(longBreakSec)
+        timerLabel.config(text="break", fg=RED)
+    elif reps % 2 == 0:
+        countDown(shortBreakSec)
+        timerLabel.config(text="break", fg=PINK)
+
+    else:
+        countDown(workSec)
+        timerLabel.config(text="work", fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
@@ -25,10 +54,20 @@ def countDown(count):
 
     countMin = math.floor(count / 60)
     countSec = count % 60
+    if countSec < 10:
+        countSec = f"0{countSec}"
 
     canvas.itemconfig(timerText, text=f"{countMin}:{countSec}")
-    if(count > 0): # makes sure we dont go to negative numbers
-        window.after(1000, countDown, count-1)  # pass in time as ms
+    if(count > 0): # makes sure we dont go to negative
+        global timer
+        timer = window.after(1000, countDown, count-1)  # pass in time as ms
+
+    else:
+        startTimer()
+        marks = ""
+        for _ in range(math.floor(reps/2)):
+            marks += "✔"
+        check.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -48,12 +87,11 @@ canvas.grid(row=2,column=2)
 start = Button(text="start", command=startTimer, bg=YELLOW, highlightbackground=YELLOW)
 start.grid(row=3,column=1)
 
-reset = Button(text="reset", bg=YELLOW, highlightbackground=YELLOW)
+reset = Button(text="reset", command=resetTimer, bg=YELLOW, highlightbackground=YELLOW)
 reset.grid(row=3,column=3,)
 
 check = Label(text="✔", fg=GREEN, bg=YELLOW, highlightbackground=YELLOW)
 check.grid(row=4,column=2)
-
 
 
 window.mainloop()
