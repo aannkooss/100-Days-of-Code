dc990f33ed67ee49a8580a09a9f37be54d31806c
Ankush Joshi <joshiankush6@gmail.com>
2025-02-26 22:48:33 -0800

    Day 29 Finish

---
+++ 100 days of python/Part2_Advanced Projects/Day 29 & 30/Day 30 - Errors, Exceptions and JSON Files/main.py
@@ -0,0 +1,14 @@
+with open("a_file.txt") as file:
+    file.read()
+
+# Key Error
+# a_dictionary = {"key" : "value"}
+# value  = a_dictionary("non_existent_key")
+
+# Index Error
+# fruit_list = ["Apple", "Banana", "Orange"]
+# fruit = fruit_list[3]
+
+# Type Error
+# text = abc
+# print(text+5)
